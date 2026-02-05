6f8dab38226b118b7310a76482db9d8fd27be0fb
modul10 <alvithops@gmail.com>
2026-02-05 14:07:28 +0700

    Update konfigurasi profile dan tambah konten 6

---
+++ latihan5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+// Menciptakan array multidimensional
+$myArray = array(
+// Default key untuk setiap elemen
+// dimulai dari 0
+array("Susi", "Budi", "Agus"),
+array("Sigit", "Tika", "Lina")
+);
+// Tampilkan informasi array
+print_r($myArray);
+echo "<hr>";
+// Tampilkan elemen array
+echo $myArray[0][0]."<br>";
+echo $myArray[0][1]."<br>";
+?>
+</body>
+</html>

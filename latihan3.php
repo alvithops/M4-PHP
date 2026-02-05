8d2d43f0921a26bb3ed2cc456e2dac80af440f74
modul10 <alvithops@gmail.com>
2026-02-05 14:04:12 +0700

    Update konfigurasi profile dan tambah konten 3

--- latihan3.php
+++ latihan3.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
-<html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <title>data pemilik kendaraan</title>
+</head>
+
 <body>
-<?php
-$pemilik["AD 92343 ZA"] = "Umar";
-$pemilik["AB 89332 NA"] = "Bakri";
-$pemilik["B 347622 BU"] = "Tika";
-$pemilik["D 88780 AS"] = "Tutik";
-$pemilik["L 87332 KL"] = "Budi";
-$index = "D 88780 AS";
-echo "Pemilik kendaraan No.Pol ".$index." adalah ".$pemilik[$index];
-?>
+
+    <?php
+    $pemilik = [
+        "AD 92343 ZA" => "umar",
+        "AB 89332 NA" => "bakri",
+        "B 347622 BU" => "tika",
+        "D 88780 AS" => "tutik",
+        "L 87332 KL" => "budi"
+    ];
+
+    $index = "D 88780 AS";
+
+    if (isset($pemilik[$index])) {
+        echo "pemilik kendaraan no.pol $index adalah {$pemilik[$index]}";
+    } else {
+        echo "data tidak ditemukan";
+    }
+    ?>
+
 </body>
+
 </html>

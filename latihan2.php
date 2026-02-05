e51c5fd4673d8b1af738ec4a07a3236e0ebd4496
modul10 <alvithops@gmail.com>
2026-02-05 14:06:36 +0700

    Update konfigurasi profile dan tambah konten 5

--- latihan2.php
+++ latihan2.php
@@ -10,10 +10,10 @@
 
     <?php
     $myarray = [];
-    $myarray[] = "universitas ";
-    $myarray[2] = "teknologi ";
-    $myarray[4] = "digital ";
-    $myarray[] = "indonesia ";
+    $myarray[] = "Universitas ";
+    $myarray[2] = "Teknologi ";
+    $myarray[4] = "Digital ";
+    $myarray[] = "Indonesia ";
 
     // menggunakan foreach agar indeks yang kosong tidak error
     foreach ($myarray as $i => $val) {
